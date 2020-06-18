da2f021ef79fd5626a4e7f616f50216537c25f2d
Daniel Yu <daniel.sm.yu@gmail.com>
2020-06-17 23:53:23 -0400

    moved Profile screen to the right

--- app/navigation/primary-navigator.tsx
+++ app/navigation/primary-navigator.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
-import { ProfileScreen, DailyScreen, BacklogScreen, AddScreen } from "../screens"
+import { DailyScreen, BacklogScreen, AddScreen, ProfileScreen } from "../screens"
 import { MaterialCommunityIcons, FontAwesome, FontAwesome5 } from "@expo/vector-icons"
 import { color, dimensions } from "../theme"
 
 type PrimaryParamList = {
-  profile: undefined
   daily: undefined
   backlog: undefined
   add: undefined
+  profile: undefined
 }
 
 const Tabs = createMaterialTopTabNavigator<PrimaryParamList>()
@@ -33,24 +33,24 @@ export function PrimaryNavigator() {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color }) => {
           const size = dimensions.icon.size
-          if (route.name === "profile") {
-            return <MaterialCommunityIcons name="account" color={color} size={size} />
-          } else if (route.name === "daily") {
+          if (route.name === "daily") {
             return <FontAwesome name="bars" color={color} size={size} />
           } else if (route.name === "backlog") {
             return <FontAwesome5 name="grip-vertical" color={color} size={size} />
           } else if (route.name === "add") {
             return <FontAwesome name="plus-square" color={color} size={size} />
+          } else if (route.name === "profile") {
+            return <MaterialCommunityIcons name="account" color={color} size={size} />
           } else {
             return null
           }
         },
       })}
     >
-      <Tabs.Screen name="profile" component={ProfileScreen} />
       <Tabs.Screen name="daily" component={DailyScreen} />
       <Tabs.Screen name="backlog" component={BacklogScreen} />
       <Tabs.Screen name="add" component={AddScreen} />
+      <Tabs.Screen name="profile" component={ProfileScreen} />
     </Tabs.Navigator>
   )
 }
