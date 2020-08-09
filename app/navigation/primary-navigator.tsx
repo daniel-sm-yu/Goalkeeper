c163a581519ed9b7c1978b319bd05e3c03c6ce6d
Daniel Yu <daniel.sm.yu@gmail.com>
2020-08-09 14:45:18 -0400

    prevented white flashes when switching screens

--- app/navigation/primary-navigator.tsx
+++ app/navigation/primary-navigator.tsx
@@ -1,6 +1,10 @@
 import React from "react"
+import { View } from "react-native"
 import { createStackNavigator } from "@react-navigation/stack"
 import { GoalsScreen, FormScreen } from "../screens"
+import { color } from "../theme"
+
+const PREVENT_FLASHES = { flex: 1, backgroundColor: color.background }
 
 export type PrimaryParamList = {
   goals: undefined
@@ -11,15 +15,17 @@ const Stack = createStackNavigator<PrimaryParamList>()
 
 export function PrimaryNavigator() {
   return (
-    <Stack.Navigator
-      initialRouteName="goals"
-      screenOptions={{
-        headerShown: false,
-        gestureEnabled: true,
-      }}
-    >
-      <Stack.Screen name="goals" component={GoalsScreen} />
-      <Stack.Screen name="form" component={FormScreen} />
-    </Stack.Navigator>
+    <View style={PREVENT_FLASHES}>
+      <Stack.Navigator
+        initialRouteName="goals"
+        screenOptions={{
+          headerShown: false,
+          animationEnabled: false,
+        }}
+      >
+        <Stack.Screen name="goals" component={GoalsScreen} />
+        <Stack.Screen name="form" component={FormScreen} />
+      </Stack.Navigator>
+    </View>
   )
 }
